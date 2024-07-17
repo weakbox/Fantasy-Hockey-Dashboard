8b4baad0155de2abd91128980d4cbe2d02693eef
weakbox <connormcleod2000@icloud.com>
2024-07-17 13:51:33 -0300

    Added year switching buttons.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const result = document.getElementById("result");
+const select2024Button = document.getElementById("select-2024-button");
+const select2025Button = document.getElementById("select-2025-button");
 
-const year = 2024;
+const year = 2025;
 
 // const draftUrl = "https://lm-api-reads.fantasy.espn.com/apis/v3/games/fhl/seasons/2024/segments/0/leagues/869377698?view=mDraftDetail&view=mSettings&view=mTeam&view=modular&view=mNav";
-// const playersUrl = "https://lm-api-reads.fantasy.espn.com/apis/v3/games/fhl/seasons/2024/players?scoringPeriodId=0&view=players_wl"
-const scoreboardUrl = `https://lm-api-reads.fantasy.espn.com/apis/v3/games/fhl/seasons/${year}/segments/0/leagues/869377698?view=modular&view=mNav&view=mMatchupScore&view=mScoreboard&view=mSettings&view=mTopPerformers&view=mTeam`
+// const playersUrl = "https://lm-api-reads.fantasy.espn.com/apis/v3/games/fhl/seasons/2024/players?scoringPeriodId=0&view=players_wl";
+const scoreboardUrl = `https://lm-api-reads.fantasy.espn.com/apis/v3/games/fhl/seasons/${year}/segments/0/leagues/869377698?view=modular&view=mNav&view=mMatchupScore&view=mScoreboard&view=mSettings&view=mTopPerformers&view=mTeam`;
 
 // Fetch schedule information:
 fetch(scoreboardUrl)
     .then((res) => res.json())
     .then((data) => {
-        console.log("Raw data: ", data);
+        // console.log("Raw data: ", data);
         displayScheduleData(data);
     })
     .catch((err) => { 
@@ -23,9 +25,9 @@ function displayScheduleData(data)
     const teamsArr = data['teams'];
     const activeMatchupPeriods = data['status']['currentMatchupPeriod'];
 
-    console.log("Schedule Array: ", scheduleArr, "Length: ", scheduleArr.length);
-    console.log("Teams Array: ", teamsArr);
-    console.log(`Found ${activeMatchupPeriods} active matchup periods.`);
+    // console.log("Schedule Array: ", scheduleArr, "Length: ", scheduleArr.length);
+    // console.log("Teams Array: ", teamsArr);
+    // console.log(`Found ${activeMatchupPeriods} active matchup periods.`);
 
     // Extract data from each matchup period one at a time.
     for (let i = 1; i <= activeMatchupPeriods; i++)
@@ -108,10 +110,35 @@ function extractMatchupData(matchup, teams)
     div.innerHTML += `<div class="matchup-container">${string}</div>`;
 }
 
+select2024Button.addEventListener("click", showData);
+select2024Button.year = 2024;
 
+select2025Button.addEventListener("click", showData);
+select2025Button.year = 2025;
 
+function showData(event)
+{
+    const year = event.target.year;
+    const url = `https://lm-api-reads.fantasy.espn.com/apis/v3/games/fhl/seasons/${year}/segments/0/leagues/869377698?view=modular&view=mNav&view=mMatchupScore&view=mScoreboard&view=mSettings&view=mTopPerformers&view=mTeam`;
 
+    clearData();
 
+    // Fetch schedule information:
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => 
+        {
+            displayScheduleData(data);
+        })
+        .catch((err) => { 
+            console.log("Error logged: ", err); 
+        });
+}
+
+function clearData()
+{
+    result.innerHTML = "";
+}
 
 // Unused code:
 
